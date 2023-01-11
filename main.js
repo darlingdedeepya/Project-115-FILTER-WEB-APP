b6cef9e870b83d83fea3121a2350ec0648cb468e
darlingdedeepya <87213449+darlingdedeepya@users.noreply.github.com>
2023-01-10 17:43:31 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,4 +1,8 @@
+noseX=0;
+noseY=0;
+
 function preload(){
+    clown_nose = loadImage('https://i.postimg.cc/7ZBcjDqp/clownnose.png');
 }
 
 function setup(){
@@ -20,15 +24,20 @@ function gotPoses(results){
     if(results.length > 0)
     {
         console.log(results);
-        console.log("nose x = " + results[0].pose.nose.x);
-        console.log("nose y = "+ results[0].pose.nose.y);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("nose x = " + noseX);
+        console.log("nose y = "+ noseY);
     }
 }
 
 function draw(){
     image(video, 0, 0, 300, 300);
+    image(clown_nose, noseX-15, noseY-15, 30, 30);
 }
 
 function take_snapshot(){
     save('myFilterImage.png')
 }
+
+
